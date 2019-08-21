ad4e2050361f97c82042ddebf480c7cf9fac2dc9
Ethan Smith <ethanfsmith@gmail.com>
2019-08-21 09:54:09 -0700

    everything needed to work on now+rds, but defining remote mysql table structure is still broken

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,14 @@ const withCSS = require('@zeit/next-css');
 module.exports = withCSS({
   target: 'serverless',
   env: {
-    BACKEND_URL: process.env.BACKEND_URL
-  }
+    BACKEND_URL: process.env.BACKEND_URL,
+  },
+  webpackDevMiddleware: config => {
+    // Solve compiling problem via vagrant
+    config.watchOptions = {
+      poll: 1000, // Check for changes every second
+      aggregateTimeout: 300, // delay before rebuilding
+    };
+    return config;
+  },
 });
